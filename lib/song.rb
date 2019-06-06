6873c2b5005f67e6a8ae3874adbf1a73454768d8
James Britton <brittonjp@gmail.com>
2019-06-06 12:29:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,18 +4,27 @@ class Song
   
   @@count = 0
   @@artists = []
+  @@genres = []
   
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
-    unless @@artists.include?(artist)
-      @@artists << artist
+    @@artists << artist
+    @@genres << genre
   end
   
   def self.count
     @@count
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genres
+    @@genres.uniq
   end
   
 end
